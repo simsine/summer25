a02bcbb6599f07ad40f39c4afaae26f4b58128dd
Simen Strømsnes <simen@simsine.no>
2025-06-24 21:00:07 +0200

    Whoopsies

--- react/app/queries/useTodoLists.ts
+++ react/app/queries/useTodoLists.ts
@@ -2,9 +2,6 @@ import { useSuspenseQuery } from "@tanstack/react-query"
 import type { TodoList } from "~/types"
 
 async function getTodoLists(): Promise<TodoList[]> {
-	setTimeout(() => {
-		
-	}, 5000);
 	const res = await fetch("/api/todolist")
 	if (!res.ok) {
 		return Promise.reject(new Error((res.statusText)))
